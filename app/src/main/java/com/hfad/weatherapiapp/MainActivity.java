ae834efd91756b156737e3aae7c6ef73c13688bb
Medkaffouh <kaffouhmohamed@gmail.com>
2022-04-03 18:06:19 +0000

    Make a standard request

--- app/src/main/java/com/hfad/weatherapiapp/MainActivity.java
+++ app/src/main/java/com/hfad/weatherapiapp/MainActivity.java
@@ -9,6 +9,16 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
@@ -32,7 +42,44 @@ public class MainActivity extends AppCompatActivity {
         btn_cityID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "You clicked me 1", Toast.LENGTH_SHORT).show();
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url = "https://www.metaweather.com/api/location/search/?query=london";
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Something wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(request);
+
+
+                // Request a string response from the provided URL.
+                //StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                //        new Response.Listener<String>() {
+                //            @Override
+                //            public void onResponse(String response) {
+                //                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                //            }
+                //        }, new Response.ErrorListener() {
+                //    @Override
+                //    public void onErrorResponse(VolleyError error) {
+                //        Toast.makeText(MainActivity.this, "Error occured", Toast.LENGTH_SHORT).show();
+                //    }
+                //});
+
+
+
+                //Toast.makeText(MainActivity.this, "You clicked me 1", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -46,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "You clicked me 3", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString()  , Toast.LENGTH_SHORT).show();
             }
         });
     }
