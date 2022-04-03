dc9b6d976ae2430638000f83fa2e0207bf2fcfe3
Medkaffouh <kaffouhmohamed@gmail.com>
2022-04-03 23:29:13 +0000

    JSONArrayRequest with Volley

--- app/src/main/java/com/hfad/weatherapiapp/WeatherDataService.java
+++ app/src/main/java/com/hfad/weatherapiapp/WeatherDataService.java
@@ -1,22 +1,26 @@
 package com.hfad.weatherapiapp;
 
 import android.content.Context;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WeatherDataService {
 
     public static final String QUERY_FOR_CITY_ID = "https://www.metaweather.com/api/location/search/?query=";
+    public static final String QUERY_FOR_CITY_WEATHER_BY_ID = "https://www.metaweather.com/api/location/";
     Context context;
     String cityID;
 
@@ -59,11 +63,69 @@ public class WeatherDataService {
         MySingleton.getInstance(context).addToRequestQueue(request);
         //return cityID;
     }
-    /*
-    public List<WeatherReportModel> getCityForecastByID(String cityID){
 
+    public interface ForeCastByIDResponse {
+        void onError(String message);
+
+        void onResponse(WeatherReportModel weatherReportModel);
     }
 
+    public void getCityForecastByID(String cityID, ForeCastByIDResponse foreCastByIDResponse){
+        List<WeatherReportModel> report = new ArrayList<>();
+
+        String url = QUERY_FOR_CITY_WEATHER_BY_ID + cityID;
+        // get the json objects
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //Toast.makeText(context, response.toString(), Toast.LENGTH_SHORT).show();
+
+                try {
+                    JSONArray consolidated_weather_list = response.getJSONArray("consolidated_weather");
+
+                    // get the first item in the array
+                    WeatherReportModel first_day = new WeatherReportModel();
+
+                    JSONObject first_day_from_api = (JSONObject) consolidated_weather_list.get(0);
+                    first_day.setId(first_day_from_api.getInt("id"));
+                    first_day.setWeather_state_name(first_day_from_api.getString("weather_state_name"));
+                    first_day.setWeather_state_abbr(first_day_from_api.getString("weather_state_abbr"));
+                    first_day.setWind_direction_compass(first_day_from_api.getString("wind_direction_compass"));
+                    first_day.setCreated(first_day_from_api.getString("created"));
+                    first_day.setMin_temp(first_day_from_api.getLong("min_temp"));
+                    first_day.setMax_temp(first_day_from_api.getLong("max_temp"));
+                    first_day.setThe_temp(first_day_from_api.getLong("the_temp"));
+                    first_day.setWind_speed(first_day_from_api.getLong("wind_speed"));
+                    first_day.setWind_direction(first_day_from_api.getLong("wind_direction"));
+                    first_day.setAir_pressure(first_day_from_api.getLong("air_pressure"));
+                    first_day.setHumidity(first_day_from_api.getInt("humidity"));
+                    first_day.setVisibility(first_day_from_api.getLong("visibility"));
+                    first_day.setPredictability(first_day_from_api.getInt("predictability"));
+
+                    foreCastByIDResponse.onResponse(first_day);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+
+        //get the property called "consolodated_weather" whichis an array
+
+        // get each item in the array and assign it to a new WeatherReportModel object.
+
+        MySingleton.getInstance(context).addToRequestQueue(request);
+    }
+/*
     public List<WeatherReportModel> getCityForecastByName(String cityName){
 
     }*/
